d0b9528f1d89ab1114f5dba10caf9d94aff8fe42
AnandhyRaju <anandhyraju@gmail.com>
2016-05-13 10:06:26 +0530

    Update ApplicationTest.java

--- app/src/androidTest/java/com/example/crystaleyes/cip/ApplicationTest.java
+++ app/src/androidTest/java/com/example/crystaleyes/cip/ApplicationTest.java
@@ -2,7 +2,7 @@ package com.example.crystaleyes.cip;
 /* Hi... i have made changes */
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
+import android.io.*
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
